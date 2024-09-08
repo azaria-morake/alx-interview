9e2519a40222601374382931666391398770e591
azaria-morake <141196419+azaria-morake@users.noreply.github.com>
2024-09-09 00:29:39 +0200

    Update 0-prime_game.py code again

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,55 +1,58 @@
-#!/usr/bin/python3
-""" Prime Game """
-
-
-def isprime(n):
-    """ Return True if n is a prime number, else False """
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-
-def delete_numbers(n, nums):
-    """ Remove numbers divisible by n from nums, set to zero """
-    nums[:] = [0 if x % n == 0 else x for x in nums]
+def sieve_of_eratosthenes(max_n):
+    """ Generate a list of primes up to max_n using the Sieve of Eratosthenes """
+    is_prime = [True] * (max_n + 1)
+    p = 2
+    while (p * p <= max_n):
+        if (is_prime[p] == True):
+            for i in range(p * p, max_n + 1, p):
+                is_prime[i] = False
+        p += 1
+    return [p for p in range(2, max_n + 1) if is_prime[p]]
 
 
 def isWinner(x, nums):
-    """ Return the name of the player that won the most rounds """
-    Maria, Ben = 0, 0
+    """ Return the name of the player who won the most rounds """
+    max_n = max(nums) if nums else 0
+    primes = sieve_of_eratosthenes(max_n)
+    
+    Maria_wins = 0
+    Ben_wins = 0
 
-    for game in range(x):
-        nums2 = list(range(1, nums[game] + 1))
+    for n in nums:
+        if n < 2:
+            Ben_wins += 1
+            continue
+        
+        # Initialize game state
+        is_prime = [True] * (n + 1)
         turn = 0
         
         while True:
-            change = False
-            for n in nums2:
-                if n > 1 and isprime(n):
-                    delete_numbers(n, nums2)
-                    change = True
+            found_move = False
+            for prime in primes:
+                if prime > n:
+                    break
+                if is_prime[prime]:
+                    found_move = True
+                    for multiple in range(prime, n + 1, prime):
+                        is_prime[multiple] = False
                     turn += 1
                     break
-
-            if not change:
+            
+            if not found_move:
                 break
         
         if turn % 2 == 0:
-            Ben += 1
+            Ben_wins += 1
         else:
-            Maria += 1
-
-    if Maria > Ben:
+            Maria_wins += 1
+    
+    if Maria_wins > Ben_wins:
         return "Maria"
-    elif Ben > Maria:
+    elif Ben_wins > Maria_wins:
         return "Ben"
     return None
+
+# Example usage
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
